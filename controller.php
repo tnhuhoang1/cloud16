7e4fd0d6a65c12db8eb5e22caeba418065114cd0
nhuhoang <nhuhoang.afk2@gmail.com>
2021-01-09 14:34:19 +0700

    fixing 1

--- controller.php
+++ controller.php
@@ -7,8 +7,11 @@
     case '/forum.php':
         require 'forum.php';
         break;
-    case '/admin/database-function.php':
-        require __DIR__.'/admin/database-function.php';
+    case '/database/database-function.php':
+        require __DIR__.'/database/database-function.php';
+        break;
+    case '/database/database-connection.php':
+        require __DIR__.'/database/database-connection.php';
         break;
     default:
         http_response_code(404);
