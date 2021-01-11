87e30ee6db510840dea1c41410c629379b22e10e
nhuhoang <nhuhoang.afk2@gmail.com>
2021-01-11 22:02:29 +0700

    fix coornator

--- android/coordinator.php
+++ android/coordinator.php
@@ -2,7 +2,7 @@
     require_once("function.php");
     if(isset($_POST['get_main_posts'])){
         $posts = array();
-        $sql = "select article_id, title, user_id, sub_cate_id from articles where is_publish = 0";
+        $sql = "select article_id, title, user_id, sub_cate_id, view_count from articles where is_publish = 0 order by article_id desc";
         $articles = simpleQuery($sql);
         foreach($articles as $v){
             $post = [];
@@ -10,6 +10,15 @@
             $user = simpleQuery($sql, 1, [$v['user_id']]);
             $sql = "select title from sub_categoris where sub_cate_id = ?";
             $subcate = simpleQuery($sql, 1, [$v['sub_cate_id']]);
+
+            $sql = "select count(user_id) as sl from like_action where article_id = ?";
+            $likes= simpleQuery($sql, 1, [$v['article_id']]);
+            
+            $likeCount = 0;
+            if(count($likes) > 0){
+                $likeCount = $likes[0]['sl'];
+            }
+
             $post['article_id'] = $v['article_id'];
             $post['title'] = $v['title'];
             $post['sub_cate_id'] = $v['sub_cate_id'];
@@ -17,6 +26,8 @@
             $post['user_id'] = $v['user_id'];
             $post['user_name'] = $user[0]['name'];
             $post['user_img'] = $user[0]['img'];
+            $post['view_count'] = $v['view_count'];
+            $post['like_count'] = $likeCount;
             array_push($posts, $post);
         
         }   
