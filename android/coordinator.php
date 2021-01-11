abdf993b2e4b56c5e56e1c0107afeb8e4a6c9611
nhuhoang <nhuhoang.afk2@gmail.com>
2021-01-11 20:18:09 +0700

    ok con de

--- android/coordinator.php
+++ android/coordinator.php
@@ -8,9 +8,12 @@
             $post = [];
             $sql = "select u.user_id, u.name as name, i.img as img from users as u, user_info as i where u.user_id = i.user_id and u.user_id = ?";
             $user = simpleQuery($sql, 1, [$v['user_id']]);
+            $sql = "select title from sub_categoris where sub_cate_id = ?";
+            $subcate = simpleQuery($sql, 1, [$v['sub_cate_id']]);
             $post['article_id'] = $v['article_id'];
             $post['title'] = $v['title'];
             $post['sub_cate_id'] = $v['sub_cate_id'];
+            $post['sub_cate_title'] = $subcate[0]['title'];
             $post['user_id'] = $v['user_id'];
             $post['user_name'] = $user[0]['name'];
             $post['user_img'] = $user[0]['img'];
